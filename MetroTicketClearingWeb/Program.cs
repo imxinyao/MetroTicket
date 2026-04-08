dfca5743221b1f6cba5a14a8b2887b249264a59e
imxinyao <q1842190658@gmail.com>
2026-04-08 12:48:10 +0800

    main

--- MetroTicketClearingWeb/Program.cs
+++ MetroTicketClearingWeb/Program.cs
@@ -1,17 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// 添加Razor Pages服务
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// 配置中间件
-app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-// 映射Razor Pages路由
 app.MapRazorPages();
 
 app.Run();
